8435520b6d1e5ae4e8317d1435e6d059bbf7e60a
Darren Cuzner <darren.cuzner@telrock.com>
2016-11-09 19:49:48 +0000

    commenting out

---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+/* tslint:disable: max-line-length */
+import { Routes } from '@angular/router';
+
+//import { DashboardComponent } from './features/dashboard.component';
+//import { NotFound404Component } from './not-found404.component';
+
+export const routes: Routes = [
+
+];
