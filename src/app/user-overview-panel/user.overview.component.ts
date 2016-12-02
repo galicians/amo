7d7cfb9b8d8b5b66e61cb577d0037dc1899f96a7
Pablo Portabales <pablo.portabales@telrock.com>
2016-12-02 12:23:50 +0000

    adding changes

--- src/app/user-overview-panel/user.overview.component.ts
+++ src/app/user-overview-panel/user.overview.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'user-overview-panel',
-	templateUrl: './user.overview.component.html';
+	templateUrl: './user.overview.component.html'
 })
 export class UserOverviewComponent {
 	
